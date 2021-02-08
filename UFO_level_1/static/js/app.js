4605740c6c7dbbea37cea0343f69ff3d6ab20d14
Haifa <haifanaj@gmail.com>
2021-02-07 21:39:25 -0600

    grab table

--- UFO_level_1/static/js/app.js
+++ UFO_level_1/static/js/app.js
@@ -1,4 +1,37 @@
 // from data.js
 var tableData = data;
 
-// YOUR CODE HERE!
+function generateTable(table, data) {
+    for (let element of data) {
+        // add a row 
+      let row = table.insertRow();
+      for (key in element) {
+          // creates a new cell
+        let cell = row.insertCell();
+        // creates a new text node
+        let text = document.createTextNode(element[key]);
+         // appends the text node to the cell
+        cell.appendChild(text);
+      }
+    }
+  };
+  // grab table and pass that to our function
+  let table = document.querySelector("table");
+  generateTable(table, tableData);
+
+// --------------------------------------------------------
+
+//   $(document).ready(function () {   
+   
+//     $('#datetime').datetimepicker({
+//         format: 'MM/DD/YYYY',
+//     });
+
+//     $('#datetime').on('dp.change', function (e) {
+//      var rex = new RegExp($(this).val(), "i");
+//     $("tr").hide();
+//     $("tr").filter(function () {
+//         return rex.test($(this).text());
+//     }).show();
+//     });  
+// });
